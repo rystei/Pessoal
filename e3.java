10326e1d96685a1c47e15e4dd23e0165a072e2d4
Gustavo <105085153+rystei@users.noreply.github.com>
2022-11-03 20:00:57 -0300

    Add files via upload

---
+++ e3.java
@@ -0,0 +1,14 @@
+public class e3 {
+    public static void main(String[] args) {
+        System.out.println("10:00");
+        for(int m = 9; m >= 0; m--) {
+
+            for(int second = 59; second >= 0; second--){
+                System.out.println(m +":"+ second);
+            }
+
+        }
+
+    }
+}
+
